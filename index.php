2fca635940c1469824663c30322354d5fbd5c452
KISHAN GUPTA <57654780+KISHANG1989@users.noreply.github.com>
2019-11-12 13:00:51 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,29 @@
-ConvertApi::setApiSecret('0UA62DeuvRLOIUAq');
-$result = ConvertApi::convert('pdf', [
-        'File' => '/path/to/my_file.docx',
-        'PdfResolution' => '606',
-        'PdfTitle' => 'outputfile',
-        'PdfSubject' => 'ddjffjf',
-        'PdfAuthor' => 'pdfonlineconvertor',
-        'PdfKeywords' => 'converted from pdfonlineconterot.com',
-    ], 'docx'
-);
-$result->saveFiles('/path/to/result/dir');
+<?php
+require __DIR__ . '/../lib/ConvertApi/autoload.php';
+
+use \ConvertApi\ConvertApi;
+
+# set your api secret
+ConvertApi::setApiSecret(getenv('0UA62DeuvRLOIUAq'));
+
+# Example of saving Word docx to PDF and to PNG
+# https://www.convertapi.com/docx-to-pdf
+# https://www.convertapi.com/docx-to-png
+
+$dir = sys_get_temp_dir();
+
+# Use upload IO wrapper to upload file only once to the API
+$upload = new \ConvertApi\FileUpload('files/test.docx');
+
+$result = ConvertApi::convert('pdf', ['File' => $upload]);
+$savedFiles = $result->saveFiles($dir);
+
+echo "The PDF saved to:\n";
+print_r($savedFiles);
+
+# Reuse the same uploaded file
+$result = ConvertApi::convert('png', ['File' => $upload]);
+$savedFiles = $result->saveFiles($dir);
+
+echo "The PNG saved to:\n";
+print_r($savedFiles);
